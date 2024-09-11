cd0a0fae560468fa566b0515a00606b4dc8fac4b
jacky_chen <altidc@qq.com>
2024-09-11 17:45:23 +0800

    fix

--- src/util/gin/middleware/recover.go
+++ src/util/gin/middleware/recover.go
@@ -7,22 +7,23 @@ import (
 	"github.com/webchen/gotools2/src/util/model2"
 )
 
-func HttpRecover(c *gin.Context) {
-	defer func() {
-		if err := recover(); err != nil {
-			msg := "系统内部错误，请联系管理员"
-			var data interface{}
-			if e, ok := err.(*model2.PanicMessage); ok {
-				msg = e.Message
-				data = e.Data
+func HttpRecover(c *gin.Context) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				msg := "系统内部错误，请联系管理员"
+				var data interface{}
+				if e, ok := err.(*model2.PanicMessage); ok {
+					msg = e.Message
+					data = e.Data
+				}
+				if e, ok := err.(string); ok {
+					msg = e
+				}
+				logs.Error(msg, data)
+				fun.SendErrorResponse(c, msg)
+				c.Abort()
 			}
-			if e, ok := err.(string); ok {
-				msg = e
-			}
-			logs.Error(msg, data)
-			fun.SendErrorResponse(c, msg)
-			c.Abort()
-		}
-	}()
-	c.Next()
+		}()
+	}
 }
