e3cc7a937dade3190e25600c41b492cded47a4dc
jacky_chen <altidc@qq.com>
2025-04-07 14:24:49 +0800

    fix image

--- src/fun/img.go
+++ src/fun/img.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"image"
 	"image/jpeg"
+	"image/png"
 	"io"
 	"os"
 	"path"
@@ -25,7 +26,7 @@ func SaveFileToLocal(url, save_path string) string {
 	ext := strings.ToLower(path.Ext(p))
 	nss := ""
 	if ext == ".jpg" || ext == ".jpeg" || ext == ".png" {
-		nss = string(CompressImageResource([]byte(ss)))
+		nss = string(CompressImageResource([]byte(ss), ext))
 	}
 	_, err := io.WriteString(f, nss)
 	if err != nil {
@@ -35,20 +36,28 @@ func SaveFileToLocal(url, save_path string) string {
 }
 
 // 压缩图片，控制在2M以内，仅仅支持 jpg/jpeg/png
-func CompressImageResource(data []byte) []byte {
+func CompressImageResource(data []byte, ext string) []byte {
 	if len(data) < 1024*1024*2 {
 		return data
 	}
-	img, _, err := image.Decode(bytes.NewReader(data))
+	var img image.Image
+	var err error
+	// 很奇怪，直接用 image.Decode，png会报格式错误，所以分开判断
+	if ext == ".png" {
+		img, err = png.Decode(bytes.NewReader(data))
+	} else {
+		img, err = jpeg.Decode(bytes.NewReader(data))
+	}
 	if err != nil {
 		return data
 	}
+
 	// 修改图片的大小
-	m := resize.Resize(0, 1080, img, resize.Lanczos3)
+	m := resize.Resize(0, 0, img, resize.Lanczos3)
 	buf := bytes.Buffer{}
 
 	// 修改图片的质量
-	err = jpeg.Encode(&buf, m, &jpeg.Options{Quality: 100})
+	err = jpeg.Encode(&buf, m, &jpeg.Options{Quality: 80})
 	if err != nil {
 		return data
 	}
