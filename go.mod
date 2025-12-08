8c27f7173c54d2fa7c1acbd1e3fab7c2de6b9796
jacky_chen <altidc@qq.com>
2025-12-08 10:13:28 +0800

    fix

--- go.mod
+++ go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/valyala/fasthttp v1.26.0
 	github.com/zouyx/agollo/v4 v4.0.8
 	golang.org/x/crypto v0.23.0
-	golang.org/x/text v0.15.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	xorm.io/xorm v1.3.9
 )
 
@@ -75,7 +75,9 @@ require (
 	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/ini.v1 v1.51.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
--- src/tool/emailtool/emailtool.go
+++ src/tool/emailtool/emailtool.go
@@ -2,54 +2,54 @@ package emailtool
 
 import (
 	"fmt"
-	"net/smtp"
+	"os"
 	"strings"
 
+	"github.com/spf13/cast"
 	"github.com/webchen/gotools2/src/base/conf"
+	"gopkg.in/gomail.v2"
 )
 
-var username = conf.GetConfig("email.sender", "altidc@qq.com").(string)
-var password = conf.GetConfig("email.password", "111111").(string)
-var host = conf.GetConfig("email.host", "smtp.qq.com:25").(string)
-var to = conf.GetConfig("email.to", "altidc@qq.com").(string)
+func SendMail(to, name, subject, body, mailType string, files []string) error {
+	from := conf.GetConfig("email.from_address", "").(string)
+	host := conf.GetConfig("email.host", "").(string)
+	port := conf.GetConfig("email.port", "").(string)
+	username := conf.GetConfig("email.username", "").(string)
+	password := conf.GetConfig("email.password", "").(string)
 
-/*
-SendMail 发送邮件
-
-username 发送者邮件
-password 授权码
-host 主机地址 smtp.qq.com:587 或 smtp.qq.com:25
-to 接收邮箱 多个接收邮箱使用 ; 隔开
-name 发送人名称
-subject 发送主题
-body 发送内容
-mailType 发送邮件内容类型
-*/
-func SendMail(to, name, subject, body, mailType string) error {
-	//return nil
-
-	hp := strings.Split(host, ":")
-	auth := smtp.PlainAuth("", username, password, hp[0])
-	var contentType string
-	if mailType == "html" {
-		contentType = "Content-Type: text/" + mailType + "; charset=UTF-8"
-	} else {
-		contentType = "Content-Type: text/plain" + "; charset=UTF-8"
+	if from == "" || host == "" || port == "" || username == "" || password == "" {
+		return fmt.Errorf("email 配置缺失")
 	}
-	msg := []byte("To: " + to + "\r\nFrom: " + name + "<" + username + ">\r\nSubject: " + subject + "\r\n" + contentType + "\r\n\r\n" + body)
-	sendTo := strings.Split(to, ";")
-	err := smtp.SendMail(host, auth, username, sendTo, msg)
-	fmt.Printf("send email result:  %+v", err)
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", from)
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", name)
+	m.SetBody("text/html", strings.ReplaceAll(body, "\n", "<br />"))
+	if len(files) > 0 {
+		for _, v := range files {
+			has, _ := fileExists(v)
+			if !has {
+				continue
+			}
+			m.Attach(v)
+		}
+	}
+	d := gomail.NewDialer(host, cast.ToInt(port), username, password)
+	err := d.DialAndSend(m)
+	if err != nil {
+		fmt.Printf("send email result:  %+v\n", err)
+	}
 	return err
-
 }
 
-// SendAlertEmail 发送报警邮件
-func SendAlertEmail(body string) {
-	SendMail(to, "网关报警", "网关报警", body, "")
-}
-
-// SendNormalEmail 普通邮件
-func SendNormalEmail(title, body string) {
-	SendMail(to, title, "网关信息", body, "")
+func fileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
